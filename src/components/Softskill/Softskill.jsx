08c8fccaba505edf99be60e1d98b8262b9932334
PtrwTg <mawngai0001@gmail.com>
2024-11-05 23:36:43 +0700

    Update CSS with hover effect for buttoncontact

--- src/components/Softskill/Softskill.jsx
+++ src/components/Softskill/Softskill.jsx
@@ -5,9 +5,11 @@ import Frame31 from './Frame31.svg'
 function Softskill() {
   return (
     <div className={styles.container}>
+      <div className={styles.containerarea}>
         <h1 className={styles.title}>Soft Skills</h1>
         <div className={styles.imgarea}>
-            <img src={Frame31} alt="" />
+            <img className={styles.img} src={Frame31} alt="" />
+            </div>
         </div>
     </div>
   )
