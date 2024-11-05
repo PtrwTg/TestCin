3d37f8e37b7f9504ffb56598f374cb2feb96c726
PtrwTg <mawngai0001@gmail.com>
2024-11-05 23:23:52 +0700

    Update CSS with hover effect for buttoncontact

--- src/components/Hardskill/Hardskill.jsx
+++ src/components/Hardskill/Hardskill.jsx
@@ -5,9 +5,11 @@ import Frame30 from './Frame30.svg'
 function Hardskill() {
   return (
     <div className={styles.container}>
+      <div className={styles.containerarea}>
         <h1 className={styles.title}>Hard Skills</h1>
         <div className={styles.imgarea}>
-            <img src={Frame30} alt="" />
+            <img className={styles.img} src={Frame30} alt="" />
+        </div>
         </div>
     </div>
   )
