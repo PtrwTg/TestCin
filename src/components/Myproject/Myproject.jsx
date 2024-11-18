f12b4e855e66f163a624bb167213d822ef71af01
PtrwTg <mawngai0001@gmail.com>
2024-11-19 01:51:46 +0700

    Refactor Skill component to use CSS background images for icons with hover animations; optimize Myproject styles for better layout and responsiveness

--- src/components/Myproject/Myproject.jsx
+++ src/components/Myproject/Myproject.jsx
@@ -1,107 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Myproject.module.css';
-import column1_1 from './column1_1.svg';
-import column1_1_ani from './column1_1_ani.svg';
-import column1_2 from './column1_2.svg';
-import column1_2_ani from './column1_2_ani.svg';
-import column2_1 from './column2_1.svg';
-import column2_1_ani from './column2_1_ani.svg';
-import column2_2 from './column2_2.svg';
-import column2_2_ani from './column2_2_ani.svg';
-import column2_3 from './column2_3.svg';
-import column2_3_ani from './column2_3_ani.svg';
-import column3_1 from './column3_1.svg';
-import column3_1_ani from './column3_1_ani.svg';
-import column3_2 from './column3_2.svg';
-import column3_2_ani from './column3_2_ani.svg';
-import column3_3 from './column3_3.svg';
-import column3_3_ani from './column3_3_ani.svg';
 
 function Myproject() {
-  const [hovered, setHovered] = useState({});
-
-  const handleMouseEnter = (column) => {
-    setHovered((prev) => ({ ...prev, [column]: true }));
-  };
-
-  const handleMouseLeave = (column) => {
-    setHovered((prev) => ({ ...prev, [column]: false }));
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.workarea}>
         <h1 className={styles.title}>My Project & Work</h1>
         <div className={styles.projectimgarea}>
           <div className={styles.rows1}>
-            <img
-              src={hovered.column1_1 ? column1_1_ani : column1_1}
-              alt="Project 1"
-              className={`${styles.image} ${styles.column1_1}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column1_1')}
-              onMouseLeave={() => handleMouseLeave('column1_1')}
-            />
-            <img
-              src={hovered.column1_2 ? column1_2_ani : column1_2}
-              alt="Project 2"
-              className={`${styles.image} ${styles.column1_2}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column1_2')}
-              onMouseLeave={() => handleMouseLeave('column1_2')}
-            />
+            <div className={`${styles.image} ${styles.column1_1}`}></div>
+            <div className={`${styles.image} ${styles.column1_2}`}></div>
           </div>
           <div className={styles.rows2}>
-            <img
-              src={hovered.column2_1 ? column2_1_ani : column2_1}
-              alt="Project 3"
-              className={`${styles.image} ${styles.column2_1}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column2_1')}
-              onMouseLeave={() => handleMouseLeave('column2_1')}
-            />
-            <img
-              src={hovered.column2_2 ? column2_2_ani : column2_2}
-              alt="Project 4"
-              className={`${styles.image} ${styles.column2_2}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column2_2')}
-              onMouseLeave={() => handleMouseLeave('column2_2')}
-            />
-            <img
-              src={hovered.column2_3 ? column2_3_ani : column2_3}
-              alt="Project 5"
-              className={`${styles.image} ${styles.column2_3}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column2_3')}
-              onMouseLeave={() => handleMouseLeave('column2_3')}
-            />
+            <div className={`${styles.image} ${styles.column2_1}`}></div>
+            <div className={`${styles.image} ${styles.column2_2}`}></div>
+            <div className={`${styles.image} ${styles.column2_3}`}></div>
           </div>
           <div className={styles.rows3}>
-            <img
-              src={hovered.column3_1 ? column3_1_ani : column3_1}
-              alt="Project 6"
-              className={`${styles.image} ${styles.column3_1}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column3_1')}
-              onMouseLeave={() => handleMouseLeave('column3_1')}
-            />
-            <img
-              src={hovered.column3_2 ? column3_2_ani : column3_2}
-              alt="Project 7"
-              className={`${styles.image} ${styles.column3_2}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column3_2')}
-              onMouseLeave={() => handleMouseLeave('column3_2')}
-            />
-            <img
-              src={hovered.column3_3 ? column3_3_ani : column3_3}
-              alt="Project 8"
-              className={`${styles.image} ${styles.column3_3}`}
-              loading="lazy"
-              onMouseEnter={() => handleMouseEnter('column3_3')}
-              onMouseLeave={() => handleMouseLeave('column3_3')}
-            />
+            <div className={`${styles.image} ${styles.column3_1}`}></div>
+            <div className={`${styles.image} ${styles.column3_2}`}></div>
+            <div className={`${styles.image} ${styles.column3_3}`}></div>
           </div>
         </div>
       </div>
--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,81 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Skill.module.css';
-import figma from './figma.svg';
-import figma_ani from './figma_ani.svg';
-import group42 from './Group42.svg';
-import group42_ani from './Group42_ani.svg';
-import group36 from './Group36.svg';
-import group36_ani from './Group36_ani.svg';
-import procreate from './procreate.svg';
-import procreate_ani from './procreate_ani.svg';
-import group37 from './Group37.svg';
-import group37_ani from './Group37_ani.svg';
-import unity from './unity.svg';
-import unity_ani from './unity_ani.svg';
 
 function Skill() {
-  const [hovered, setHovered] = useState({});
-
-  const handleMouseEnter = (icon) => {
-    setHovered((prev) => ({ ...prev, [icon]: true }));
-  };
-
-  const handleMouseLeave = (icon) => {
-    setHovered((prev) => ({ ...prev, [icon]: false }));
-  };
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Software Skills</h1>
       <div className={styles.imgArea}>
-        <img
-          src={hovered.figma ? figma_ani : figma}
-          alt="Figma"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('figma')}
-          onMouseLeave={() => handleMouseLeave('figma')}
-        />
-        <img
-          src={hovered.group42 ? group42_ani : group42}
-          alt="Group42"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('group42')}
-          onMouseLeave={() => handleMouseLeave('group42')}
-        />
-        <img
-          src={hovered.group36 ? group36_ani : group36}
-          alt="Group36"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('group36')}
-          onMouseLeave={() => handleMouseLeave('group36')}
-        />
-        <img
-          src={hovered.procreate ? procreate_ani : procreate}
-          alt="Procreate"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('procreate')}
-          onMouseLeave={() => handleMouseLeave('procreate')}
-        />
-        <img
-          src={hovered.group37 ? group37_ani : group37}
-          alt="Group37"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('group37')}
-          onMouseLeave={() => handleMouseLeave('group37')}
-        />
-        <img
-          src={hovered.unity ? unity_ani : unity}
-          alt="Unity"
-          className={`${styles.icon}`}
-          loading="lazy"
-          onMouseEnter={() => handleMouseEnter('unity')}
-          onMouseLeave={() => handleMouseLeave('unity')}
-        />
+        <div className={`${styles.icon} ${styles.figma}`}></div>
+        <div className={`${styles.icon} ${styles.group42}`}></div>
+        <div className={`${styles.icon} ${styles.group36}`}></div>
+        <div className={`${styles.icon} ${styles.procreate}`}></div>
+        <div className={`${styles.icon} ${styles.group37}`}></div>
+        <div className={`${styles.icon} ${styles.unity}`}></div>
       </div>
     </div>
   );
