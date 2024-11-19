1355f65d4499445b8029d91c21bb53bc81f100e8
PtrwTg <mawngai0001@gmail.com>
2024-11-20 01:02:47 +0700

    Remove Project2 component and associated styles to streamline the application

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home/Home';
 import Project1 from './pages/Project1/Project1';
-import Project2 from './pages/Project2/Project2';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Project1" element={<Project1 />} />
-        <Route path="/Project2" element={<Project2 />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
