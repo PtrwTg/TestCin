865a4d339d1db69b27829e73ba1e143e434b5a9a
PtrwTg <mawngai0001@gmail.com>
2024-11-19 10:42:54 +0700

    Implement routing for Project1 and Project2; update Myproject component to use links for navigation

--- src/components/Myproject/Myproject.jsx
+++ src/components/Myproject/Myproject.jsx
@@ -1,5 +1,6 @@
-//src/conponents/Myproject/Myproject.jsx
+// src/components/Myproject/Myproject.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Myproject.module.css';
 
 function Myproject() {
@@ -9,8 +10,8 @@ function Myproject() {
         <h1 className={styles.title}>My Project & Work</h1>
         <div className={styles.projectimgarea}>
           <div className={styles.rows1}>
-            <div className={`${styles.image} ${styles.column1_1}`}></div>
-            <div className={`${styles.image} ${styles.column1_2}`}></div>
+            <Link to="/Project1" className={`${styles.image} ${styles.column1_1}`}></Link>
+            <Link to="/Project2" className={`${styles.image} ${styles.column1_2}`}></Link>
           </div>
           <div className={styles.rows2}>
             <div className={`${styles.image} ${styles.column2_1}`}></div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-//main.jsx
+// main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home/Home';
-import Test from './pages/Test';
+import Project1 from './pages/Project1/Project1';
+import Project2 from './pages/Project2/Project2';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Project1" element={<Project1 />} />
+        <Route path="/Project2" element={<Project2 />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
